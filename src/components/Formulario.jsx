6694552e6ad6fbe7a5f13250b6428f1bb330235f
Marcos Ruiz <marcos_1510@hotmail.com>
2022-05-18 12:56:27 +0200

    primer commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,186 @@
+import React from "react";
+import Alerta from "./Alerta";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+const Formulario = ({ cliente, cargando }) => {
+  const navigate = useNavigate();
+
+  const nuevoClienteSchema = Yup.object().shape({
+    nombre: Yup.string()
+      .min(3, "El nombre es muy corto")
+      .max(20, "El nombre es muy largo")
+      .required("El Nombre del Cliente es obligatorio"),
+    empresa: Yup.string().required("El nombre de la empresa es obligatorio"),
+    email: Yup.string()
+      .email("Email no valido")
+      .required("El email es obligatorio"),
+    telefono: Yup.number()
+      .integer("Numero no valido")
+      .positive("Numero no valido")
+      .typeError("El numero no es valido"),
+  });
+
+  const handleSubmit = async (values) => {
+    try {
+      let respuesta
+      if (cliente.id) {
+        //editando un registro
+        const url = `http://localhost:4000/clientes/${cliente.id}`;
+
+        respuesta = await fetch(url, {
+          method: "PUT",
+          body: JSON.stringify(values),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      } else {
+        //nuevo registro
+        const url = "http://localhost:4000/clientes";
+
+        respuesta = await fetch(url, {
+          method: "POST",
+          body: JSON.stringify(values),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      }
+
+      await respuesta.json();
+      navigate("/clientes");
+
+      
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return cargando ? (
+    <Spinner />
+  ) : (
+    <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto">
+      <h1 className="text-gray-600 font-bold text-xl uppercase text-center">
+        {cliente?.nombre ? "Editar cliente" : "Agregar cliente"}
+      </h1>
+
+      <Formik
+        initialValues={{
+          nombre: cliente.nombre ? cliente.nombre : "",
+          empresa: cliente?.empresa ?? "",
+          email: cliente?.email ?? "",
+          telefono: cliente?.telefono ?? "",
+          notas: cliente?.notas ?? "",
+        }}
+        enableReinitialize={true}
+        onSubmit={async (values, { resetForm }) => {
+          await handleSubmit(values);
+
+          resetForm();
+        }}
+        validationSchema={nuevoClienteSchema}
+      >
+        {({ errors, touched }) => {
+          //console.log(errors)
+          return (
+            <Form className="mt-10">
+              <div className="mb-4">
+                <label className="text-gray-800" htmlFor="nombre">
+                  Nombre:
+                </label>
+                <Field
+                  id="nombre"
+                  type="text"
+                  className="mt-2 block w-full p-3 bg-gray-50"
+                  placeholder="Nombre del cliente"
+                  name="nombre"
+                />
+                {errors.nombre && touched.nombre ? (
+                  <Alerta>{errors.nombre}</Alerta>
+                ) : null}
+              </div>
+
+              <div className="mb-4">
+                <label className="text-gray-800" htmlFor="empresa">
+                  Empresa:
+                </label>
+                <Field
+                  id="empresa"
+                  type="text"
+                  className="mt-2 block w-full p-3 bg-gray-50"
+                  placeholder="Nombre de la empresa"
+                  name="empresa"
+                />
+                {errors.empresa && touched.empresa ? (
+                  <Alerta>{errors.empresa}</Alerta>
+                ) : null}
+              </div>
+
+              <div className="mb-4">
+                <label className="text-gray-800" htmlFor="email">
+                  Email:
+                </label>
+                <Field
+                  id="email"
+                  type="email"
+                  className="mt-2 block w-full p-3 bg-gray-50"
+                  placeholder="Email del cliente"
+                  name="email"
+                />
+                {errors.email && touched.email ? (
+                  <Alerta>{errors.email}</Alerta>
+                ) : null}
+              </div>
+
+              <div className="mb-4">
+                <label className="text-gray-800" htmlFor="telefono">
+                  Telefono:
+                </label>
+                <Field
+                  id="telefono"
+                  type="tel"
+                  className="mt-2 block w-full p-3 bg-gray-50"
+                  placeholder="Telefono del cliente"
+                  name="telefono"
+                />
+                {errors.telefono && touched.telefono ? (
+                  <Alerta>{errors.telefono}</Alerta>
+                ) : null}
+              </div>
+
+              <div className="mb-4">
+                <label className="text-gray-800" htmlFor="notas">
+                  Notas:
+                </label>
+                <Field
+                  as="textarea"
+                  id="notas"
+                  type="text"
+                  className="mt-2 block w-full p-3 bg-gray-50 h-40"
+                  placeholder="Notas del cliente"
+                  name="notas"
+                />
+              </div>
+
+              <input
+                type="submit"
+                value={cliente?.nombre ? "Editar cliente" : "Agregar cliente"}
+                className="mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg"
+              />
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
+};
+
+Formulario.defaultProps = {
+  cliente: {},
+  cargando: false,
+};
+
+export default Formulario;
---
+++ src/paginas/VerCliente.jsx
@@ -0,0 +1,89 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Spinner from "../components/Spinner";
+
+const VerCliente = () => {
+  const [cliente, setCliente] = useState({});
+  const [cargando, setCargando] = useState(true);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const obtenerClienteAPI = async () => {
+      try {
+        const url = `http://localhost:4000/clientes/${id}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setCliente(resultado);
+      } catch (error) {
+        console.log(error);
+      }
+      setTimeout(() => {
+        setCargando(!cargando);
+      }, 1000);
+    };
+    obtenerClienteAPI();
+  }, []);
+
+  return cargando ? (
+    <Spinner />
+  ) : Object.keys(cliente).length === 0 ? (
+    <p>No hay resultados</p>
+  ) : (
+    <div>
+      {cargando ? (
+        "Cargando..."
+      ) : (
+        <>
+          <h1 className="font-black text-4xl text-blue-900">
+            Ver Cliente: {cliente.nombre}
+          </h1>
+          <p className="mt-3">Informacion del cliente</p>
+
+          {cliente.nombre && (
+            <p className="text-4xl text-gray-600 mt-10">
+              <span className="uppercase font-bold text-gray-800">
+                Cliente:
+              </span>
+              {cliente.nombre}
+            </p>
+          )}
+
+          {cliente.email && (
+            <p className="text-2xl text-gray-600 mt-4">
+              <span className="uppercase font-bold text-gray-800">Email:</span>
+              {cliente.email}
+            </p>
+          )}
+
+          {cliente.telefono && (
+            <p className="text-2xl text-gray-600 mt-4">
+              <span className="uppercase font-bold text-gray-800">
+                Telefono:
+              </span>
+              {cliente.telefono}
+            </p>
+          )}
+
+          {cliente.empresa && (
+            <p className="text-2xl text-gray-600 mt-4">
+              <span className="uppercase font-bold text-gray-800">
+                Empresa:
+              </span>
+              {cliente.empresa}
+            </p>
+          )}
+
+          {cliente.notas && (
+            <p className="text-2xl text-gray-600 mt-4">
+              <span className="uppercase font-bold text-gray-800">Notas:</span>
+              {cliente.notas}
+            </p>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default VerCliente;
